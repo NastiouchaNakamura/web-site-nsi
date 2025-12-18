087bb9929d7ad23f9ecdc542f535565834c56fe0
Anaël <anael@barodine.fr>
2025-12-18 01:55:23 +0100

    feat: Finalement le site sera en material mkdocs c plus simple.

--- includes/footer.php
+++
@@ -1,7 +0,0 @@
-<footer>
-    <?php // TODO : Gérer la licence et l'attribution et autres trucs ennuyeux ?>
-    <p>
-        <small>Site Web développé par Anaël <span style="font-variant: small-caps;">Barodine</span> | <a href="https://github.com/NastiouchaNakamura/web-site-nsi" target="_blank">Version 0.1</a></small>
-    </p>
-    <!-- Tous mes sites sont open-source et faits avec amour <3 -->
-</footer>
